15d4d1a50e9fcdf0ceed4f550792f48ef36f230a
Friendo Polythemus <whowasitpolythemus@protonmail.com>
2020-09-02 00:35:08 -0700

    updating entry path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'main.js'),
+  entry: path.resolve(__dirname, 'src', 'public', 'main.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
