543d7bc314279d52aca0f4224c6ecf993cc3afe0
Friendo Polythemus <whowasitpolythemus@protonmail.com>
2020-09-02 19:21:05 -0700

    Helpfiles component, making statblock component

--- src/public/components/App.vue
+++ src/public/components/App.vue
@@ -1,35 +1,55 @@
 <template>
-  <div id="app">
-  <h1>Helpfiles</h1>
+  <div>
   <ul>
-    <li v-for="helpfile in helpfiles">
-      <h2>{{ helpfile.category }}</h2>
-     <ul v-for="entry in helpfile.entries">
-        <li>
-          <h3>{{ entry.name }}</h3>
-          <div v-html="entry.contents" />
-        </li>
-      </ul>
+    <li v-for="stat in attributes.physical">
+      {{stat.name}}
+      <input 
+        v-for="n in 5"
+        :id="'attr' + stat.name + n"
+        name="'attr-' + stat.name'"
+        type="radio"
+      />
+      <label
+        v-for="n in 5"
+        :id="'value'+ stat.name + n"
+        :for="'attr' + stat.name + n"
+      > {{n}}
+      </label>
     </li>
   </ul>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+  // import Component from 'filepath';
+  // The export default syntax is declaring a component 
+  // which can be registered and reused later.
 
-// The export default syntax is declaring a component 
-// which can be registered and reused later.
   export default {
     name: 'App',
+    components: {},
     data () {
       return {
-        helpfiles: {}
+        attributes: {
+          physical: [
+            {name: 'Strength', value: 1},
+            {name: 'Dexterity', value: 1},
+            {name: 'Stamina', value: 1}
+          ],
+          social: [
+            {name: 'Charisma', value: 1},
+            {name: 'Manipulation', value: 1},
+            {name: 'Appearance', value: 1}
+          ],
+          mental: [
+            {name: 'Intelligence', value: 1},
+            {name: 'Wits', value: 1},
+            {name: 'Perception', value: 1},   
+          ]
+        }
       }
     },
     created() {
-       axios.get('/api/helpfiles')
-      .then(res => this.helpfiles = res.data)
     }
   }
 </script>
---
+++ src/public/components/Helpfiles.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+  <h1>Helpfiles</h1>
+  <ul>
+    <li v-for="helpfile in helpfiles">
+      <h2>{{ helpfile.category }}</h2>
+     <ul v-for="entry in helpfile.entries">
+        <li>
+          <h3>{{ entry.name }}</h3>
+          <div v-html="entry.contents" />
+        </li>
+      </ul>
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+// The export default syntax is declaring a component 
+// which can be registered and reused later.
+  export default {
+    name: 'Helpfiles',
+    data () {
+      return {
+        helpfiles: {}
+      }
+    },
+    created() {
+       axios.get('/api/helpfiles')
+      .then(res => this.helpfiles = res.data)
+    }
+  }
+</script>
