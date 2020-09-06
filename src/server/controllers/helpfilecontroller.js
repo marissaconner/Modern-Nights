e1db1ebb5333448a17b5699c4abfdc13b0ba1b9a
Marissa Conner <marissafromtexas@gmail.com>
2020-09-05 18:19:39 -0700

    Search method

--- src/server/controllers/helpfilecontroller.js
+++ src/server/controllers/helpfilecontroller.js
@@ -9,7 +9,20 @@ module.exports = {
       }
     })
   },
-  
+
+  search: function(client, input, callback) {
+    let  searchString = input.replace(/\W/g, '').toLowerCase();
+
+    const queryString = `select json_agg( json_build_object('name', name, 'contents', contents)) as entries from helpfiles where lower(contents) like '%%${searchString}%%' group by category order by category asc)`;
+    client.query(queryString, (err, data) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, data.rows)
+      }
+    })
+  },
+
   getBuckets: function(client, callback) {
     const queryString = `select distinct bucket, false as selected from helpfiles`
     client.query(queryString, (err, data) => {
