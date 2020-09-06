da6365f373fc175b741d863ce4a897c3292ae8f7
Marissa Conner <marissafromtexas@gmail.com>
2020-09-05 18:01:41 -0700

    Removing bucket filter

--- src/server/controllers/helpfilecontroller.js
+++ src/server/controllers/helpfilecontroller.js
@@ -1,6 +1,6 @@
 module.exports = {
   getHelpfiles: function(client, callback) {
-    const queryString = `select category, json_agg( json_build_object('name', name, 'contents', contents)) as entries from helpfiles where bucket='help' group by category order by category asc`;
+    const queryString = `select category, json_agg( json_build_object('name', name, 'contents', contents)) as entries from helpfiles group by category order by category asc`;
     client.query(queryString, (err, data) => {
       if (err) {
         callback(err, null);
@@ -9,18 +9,7 @@ module.exports = {
       }
     })
   },
-
-   getRulefiles: function(client, callback) {
-    const queryString = `select category, array_agg(name) as entries from helpfiles where bucket='rules' group by category order by category asc`;
-    client.query(queryString, (err, data) => {
-      if (err) {
-        callback(err, null);
-      } else {
-        callback(null, data.rows)
-      }
-    })
-  }, 
-
+  
   getBuckets: function(client, callback) {
     const queryString = `select distinct bucket, false as selected from helpfiles`
     client.query(queryString, (err, data) => {
